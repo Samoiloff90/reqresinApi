f00f7ad169d1b88558a9aa57f0fe84b6898f0095
Eugene Samoilov <e.e.samoilov@mail.ru>
2022-04-14 21:43:29 +0300

    start project

---
+++ src/test/java/apiTests/ReqresinTests.java
@@ -0,0 +1,42 @@
+package apiTests;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
+import static org.hamcrest.Matchers.is;
+
+public class ReqresinTests {
+    @Test
+    void sucessfulLogin() {
+
+        String authorizedData = "{\"email\": \"eve.holt@reqres.in\", " +
+                "\"password\": \"cityslicka\"}";
+
+        given()
+                .body(authorizedData)
+                .contentType(JSON)
+                .when()
+                .post("https://reqres.in/api/login")
+                .then()
+                .statusCode(200)
+                .body("token", is("QpwL5tke4Pnpja7X4"));
+    }
+
+    @Test
+    void missingPasswoedLogin() {
+
+        String authorizedData = "{\"email\": \"eve.holt@reqres.in\"}";
+
+        given()
+                .body(authorizedData)
+                .contentType(JSON)
+                .when()
+                .post("https://reqres.in/api/login")
+                .then()
+                .statusCode(400)
+                .body("error", is("Missing password"));
+    }
+
+
+}
